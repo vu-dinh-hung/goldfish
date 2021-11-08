caf5f7cbee8b319b32584c63dff8cda097af6702
GuldSchlaahga <33758782+GuldSchlaahga@users.noreply.github.com>
2021-11-07 20:50:14 -0500

    Update filesystem.rs

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,15 +1,15 @@
-pub fn read_file_test(){
+pub fn test_1_read_file(){
 	asserteq!(dvcs.read_file(fileContainingHelloWorld), Ok("Hello World"))
 }
-pub fn write_file_test(){
+pub fn test_2_write_file(){
 	asserteq!(dvcs.write_file(invalidpath, somedata), Err("InvalidPathError"))
 }
-pub fn move_file_test(){
+pub fn test_3_move_file(){
 
 	asserteq!(dvcs.move_file(sourcepath, destpath, c=true), true)
 	asserteq!(dvcs.read_file(sourcepath), dvcs.read_file(destpath))
 }
-pub fn remove_file_test(){
+pub fn test_4_remove_file(){
 	asserteq!(dvcs.remove_file(path, r=true), true)
 	pathprime = path + "/filename"
 	asserteq!(dvcs.write_file(pathprime), Err("InvalidPathError"))
