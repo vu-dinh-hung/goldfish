20a84a017575329181171eae0ae660753493c916
Hung Vu <vudinhhung942k@gmail.com>
2021-11-14 22:36:53 -0500

    Add write_file, read_file, polish other filesystem functions

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,44 +1,52 @@
 //! Filesystem
-//! Note that this module uses std::io::Result, not the Result class from prelude (std::result::Result).
-use std::io::Result;
+use std::io;
 use std::path::Path;
 use std::fs;
 
 
-pub fn save_file(data: &str, path: &str) -> Result<()> {
-    todo!()
+pub fn write_file(data: &str, path: &str) -> io::Result<()> {
+    //! Write data to the specified file
+    //! If the file does not exist, create the file as well as all intermediate parent folders
+    let parent_folder = parent(path);
+    match parent_folder {
+        Some(folder) => {
+            if !is_directory(folder.as_str()) {
+                create_directory(folder.as_str())?  // return early with the IO error if this errors out
+            }
+            fs::write(path, data)
+        }
+        None => Err(io::Error::new(io::ErrorKind::Other, "Cannot create the specified path"))
+    }
 }
 
-pub fn read_file(path: &str) -> Result<String> {
-    todo!()
+pub fn read_file(path: &str) -> io::Result<String> {
+    fs::read_to_string(path)
 }
 
-pub fn create_directory(path: &str) -> Result<()> {
+pub fn create_directory(path: &str) -> io::Result<()> {
+    //! Create the given path, including all intermediate directories
     fs::create_dir_all(path)
 }
 
-pub fn remove_file(path: &str, recursive: bool) -> Result<()> {
+pub fn remove_file(path: &str, recursive: bool) -> io::Result<()> {
     todo!()
 }
 
-pub fn move_file(source: &str, dest: &str, recursive: bool, copy: bool) -> Result<()> {
+pub fn move_file(source: &str, dest: &str, recursive: bool, copy: bool) -> io::Result<()> {
     todo!()
 }
 
 pub fn is_directory(path: &str) -> bool {
+    //! Check if path is a directory
     Path::new(path).is_dir()
 }
 
 pub fn is_file(path: &str) -> bool {
+    //! Check if path is a file
     Path::new(path).is_file()
 }
 
 pub fn parent(path: &str) -> Option<String> {
-    match Path::new(path).parent() {
-        Some(path_obj) => match path_obj.to_str() {
-            Some(path_string) => Some(path_string.to_string()),
-            None => None
-        }
-        None => None
-    }
+    //! Get the parent path of the given path
+    Path::new(path).parent()?.to_str().map(|path_string| path_string.to_string())
 }
