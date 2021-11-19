b66f4ff4b50090dfd72f0ade3be6fb37796468ae
Hung Vu <vudinhhung942k@gmail.com>
2021-11-18 20:52:19 -0500

    Add utilities::hash()

--- src/utilities.rs
+++ src/utilities.rs
@@ -3,11 +3,18 @@ use std::path::PathBuf;
 use std::fs;
 use std::env;
 use nanoid::nanoid;
+use sha2::{Sha256, Digest};
 
 pub fn diff(lines1: Vec<String>, lines2: Vec<String>) -> Vec<(String, String)> {
     //! Returns a list of differences (line by line) in the two strings
     //! (e.g. [('+', 'this_line_only_in_2\n'), ('=', 'common_line\n'), ('-', 'this_line_not_in_2\n)])
     todo!()
+}
+
+pub fn hash(data: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(data);
+    format!("{:X}", hasher.finalize())
 }
 
 pub fn generate_id() -> String {
