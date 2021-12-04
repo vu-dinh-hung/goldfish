ceee49777ba0ad0818c754c1998ba7814ac5a58f
Usman Amin <us.sukhera@gmail.com>
2021-12-04 16:10:56 -0500

    added connect_to_server function

--- src/networking.rs
+++ src/networking.rs
@@ -1,29 +1,28 @@
-// Crate for serializing
 use ssh2::Session;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use toml::Value;
 
-
-#[test]
-pub fn test_1_connect_ssh() {
+fn connect_to_server(ip_address: &str, username: &str, password: &str) -> Session {
     // Replace with IP address of remote
-    let tcp = TcpStream::connect("127.0.0.1:22").unwrap();
+    let tcp = TcpStream::connect(ip_address).unwrap();
     let mut session = Session::new().unwrap();
     session.set_tcp_stream(tcp);
     session.handshake().unwrap();
     // Replace with login credentials
-    session.userauth_password("username", "password").unwrap();
-    assert!(session.authenticated());
-    let mut channel = session.channel_session().unwrap();
-    channel.exec("ls").unwrap();
-    let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    println!("{}", s);
-    channel.wait_close().ok();
-    println!("{}", channel.exit_status().unwrap());
-    
+    session.userauth_password(username, password).unwrap();
+    return session;
 }
+
+
+// fn write_to_context(data: Value, context: ServerContext) -> Result<u32, u32> {
+//     todo!()
+// }
+// fn push_to_server(context: ServerContext) -> Result<u32, u32> {
+//     todo!()
+// }
+
+
 
 // pub fn test_2_write_file(){
 // 	assert_eq!(dvcs.write_file(invalidpath, somedata), Err("InvalidPathError"))
@@ -42,12 +41,31 @@ pub fn test_1_connect_ssh() {
 // fn start_server() -> Result<u32, u32> {
 //     todo!()
 // }
-// fn connect_to_server(ip_address: &String, port: &u32) -> ServerContext {
-//     todo!()
-// }
-// fn write_to_context(data: Value, context: ServerContext) -> Result<u32, u32> {
-//     todo!()
-// }
-// fn push_to_server(context: ServerContext) -> Result<u32, u32> {
-//     todo!()
-// }
+
+#[cfg(test)]
+mod tests {
+    use ssh2::Session;
+    use std::net::TcpStream;
+    use std::io::prelude::*;
+
+    #[test]
+    pub fn test_1_connect_ssh() {
+        // Replace with IP address of remote
+        let tcp = TcpStream::connect("127.0.0.1:22").unwrap();
+        let mut session = Session::new().unwrap();
+        session.set_tcp_stream(tcp);
+        session.handshake().unwrap();
+        // Replace with login credentials
+        session.userauth_password("username", "password").unwrap();
+        assert!(session.authenticated());
+        let mut channel = session.channel_session().unwrap();
+        channel.exec("ls").unwrap();
+        let mut s = String::new();
+        channel.read_to_string(&mut s).unwrap();
+        println!("{}", s);
+        channel.wait_close().ok();
+        println!("{}", channel.exit_status().unwrap());
+        
+    }
+}
+
