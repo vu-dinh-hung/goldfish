6eafde954070181bd63c043005a1687d7a88f9e1
Usman Amin <us.sukhera@gmail.com>
2021-12-04 22:44:35 -0500

    added download_from_server method

--- src/networking.rs
+++ src/networking.rs
@@ -1,17 +1,43 @@
 use ssh2::Session;
+use ssh2::Error;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use toml::Value;
+use std::path::Path;
 
-fn connect_to_server(ip_address: &str, username: &str, password: &str) -> Session {
-    // Replace with IP address of remote
+// Returns SSH connection to ip_address with specified credentials. 
+// ip_address must include the port number
+fn connect_to_server(ip_address: &str, username: &str, password: &str) -> Option<Session> {
     let tcp = TcpStream::connect(ip_address).unwrap();
     let mut session = Session::new().unwrap();
     session.set_tcp_stream(tcp);
     session.handshake().unwrap();
-    // Replace with login credentials
     session.userauth_password(username, password).unwrap();
-    return session;
+    if session.authenticated() {
+        return Some(session);
+    } else {
+        return None;
+    }
+    
+}
+
+// Download file at path from given server session in to current directory
+fn download_from_server(session: Session, path: &str) -> Result<&str, Error> {
+    let downloaded = session.scp_recv(Path::new(path));
+    match downloaded {
+        Ok((mut remote_file, stat)) => {
+            let mut contents = Vec::new();
+            remote_file.read_to_end(&mut contents).unwrap();
+        
+            // Close the channel and wait for the whole content to be tranferred
+            remote_file.send_eof().unwrap();
+            remote_file.wait_eof().unwrap();
+            remote_file.close().unwrap();
+            remote_file.wait_close().unwrap();
+            Ok("File downloaded.")
+        },
+        Err(e) => Err(e),
+    }
 }
 
 
@@ -49,14 +75,14 @@ mod tests {
     use std::io::prelude::*;
 
     #[test]
-    pub fn test_1_connect_ssh() {
-        // Replace with IP address of remote
+    pub fn test_1_connect_ssh_csug() {
+        // Replace with IP address of CSUG machine
         let tcp = TcpStream::connect("127.0.0.1:22").unwrap();
         let mut session = Session::new().unwrap();
         session.set_tcp_stream(tcp);
         session.handshake().unwrap();
         // Replace with login credentials
-        session.userauth_password("username", "password").unwrap();
+        session.userauth_password("netid", "password").unwrap();
         assert!(session.authenticated());
         let mut channel = session.channel_session().unwrap();
         channel.exec("ls").unwrap();
